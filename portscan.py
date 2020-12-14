1b48256412f9ba9850061f1e81a6f93dd23f7e67
zHeadCrusher <67909733+zHeadCrusher@users.noreply.github.com>
2020-12-14 15:10:31 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 import socket
+from colorama import Fore
 
-ip = raw_input("Digite o ip do alvo: ")
+ip = raw_input("Target IP: ")
 for porta in range(1,65535):
-	s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	if s.connect_ex((ip, porta)) == 0:
-		print "Porta",porta,"[ABERTA]"
-		s.close()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if s.connect_ex((ip, porta)) == 0:
+                print (Fore.GREEN)
+                print "[+] Port",porta,"is Opened"
+                s.close()
